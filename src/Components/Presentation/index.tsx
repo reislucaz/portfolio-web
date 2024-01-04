3cf6a32f9950e890d357d3bc0938c4b3239e5dd1
Lucas Reis <reislucaz@DESKTOP-NS1A866>
2024-01-04 00:08:33 -0300

    feat: :sparkles: 5° Período! ;D

--- src/Components/Presentation/index.tsx
+++ src/Components/Presentation/index.tsx
@@ -1,27 +1,30 @@
-import './index.css'
-import LucasImg from './lucas-reis.webp'
+import "./index.css";
+import LucasImg from "./lucas-reis.webp";
 
-function Presentation () {
-    return (
-        <div className="main-presentation">
-            <div className="presentation-container">
-                <h2 id="sobre-mim-text">_Sobre mim</h2>
-                <div className="photo-text">
-                    <img src={LucasImg} alt="" id="lucas-photo" />
-                    <div className="lucas-text">
-                        <p>Lucas Reis, Estudante do 3º Período de Engenharia de Software na instituição UNIEvangélica.
-                        Atualmente trabalho como Desenvolvedor Full-Stack na Kingspan Isoeste.
-                        Possuo conhecimento de inglês em nível intermediário, conhecimento sobre processos de software,
-                        arquitetura e desenvolvimento.
-                        </p>
-                        <p id="final-text">
-                        Tenho paixão por aprender e acredito que a vida seja uma construção de aprendizados contínuos._
-                        </p>
-                    </div>
-                </div>
-            </div>
+function Presentation() {
+  return (
+    <div className="main-presentation">
+      <div className="presentation-container">
+        <h2 id="sobre-mim-text">_Sobre mim</h2>
+        <div className="photo-text">
+          <img src={LucasImg} alt="" id="lucas-photo" />
+          <div className="lucas-text">
+            <p>
+              Lucas Reis, Estudante do 5º Período de Engenharia de Software na
+              instituição UNIEvangélica. Atualmente trabalho como Desenvolvedor
+              Full-Stack na Kingspan Isoeste. Possuo conhecimento de inglês em
+              nível intermediário, conhecimento sobre processos de software,
+              arquitetura e desenvolvimento.
+            </p>
+            <p id="final-text">
+              Tenho paixão por aprender e acredito que a vida seja uma
+              construção de aprendizados contínuos._
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Presentation
+export default Presentation;
